efc4442a56d3719f723771202564098c68e62b6a
Felix Dorn <github@felixdorn.fr>
2022-02-13 13:34:24 +0000

    Update RickRollServiceProvider.php

--- src/RickRollServiceProvider.php
+++ src/RickRollServiceProvider.php
@@ -8,8 +8,6 @@ class RickRollServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(LaravelRickRoll::class, function () {
-            return new LaravelRickRoll();
-        });
+       //
     }
 }
